03d35f6ab9b5148b41be76581ca4adf050c6607a
izanhzh <hzh990094740@outlook.com>
2025-08-13 16:54:40 +0800

    Resolve JSON serialization issue

--- src/PowCapServer.Core/Models/ChallengeTokenInfo.cs
+++ src/PowCapServer.Core/Models/ChallengeTokenInfo.cs
@@ -2,6 +2,15 @@ namespace PowCapServer.Models;
 
 public record ChallengeTokenInfo
 {
+#if NETSTANDARD2_0
+    public ChallengeTokenInfo()
+    {
+        Challenge = new Challenge(0, 0, 0);
+        Token = string.Empty;
+        Expires = 0;
+    }
+#endif
+
     public ChallengeTokenInfo(Challenge challenge, string token, long expires)
     {
         Challenge = challenge;
