d0a7fca5b8f76d8e6bd00bd4491cbddd1ca85022
izanhzh <hzh990094740@outlook.com>
2026-06-08 18:38:31 +0800

    fix fails with JSON deserialization error

--- src/PowCapServer.Core/Models/ChallengeSolution.cs
+++ src/PowCapServer.Core/Models/ChallengeSolution.cs
@@ -4,13 +4,11 @@ namespace PowCapServer.Models;
 
 public class ChallengeSolution
 {
-#if NETSTANDARD2_0
     public ChallengeSolution()
     {
         Token = string.Empty;
-        Solutions = new List<int>();
+        Solutions = [];
     }
-#endif
 
     public ChallengeSolution(string token, IList<int> solutions)
     {
